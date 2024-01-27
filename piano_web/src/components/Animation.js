54ee9d092a3caf7240ff50b51a94355616087f8e
Mason <masondary@gmail.com>
2024-01-27 14:30:55 -0500

    adjusted layout

--- piano_web/src/components/Animation.js
+++ piano_web/src/components/Animation.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
 
-const SimpleAnimation = () => {
+const SimpleAnimation = ({ paperSize }) => {
   const [keys, setKeys] = useState([]);
   useEffect(() => {
     keys.forEach((key, index) => {
       if (key) {
         key.animate([
-          { transform: 'translateY(-10vw)', opacity: 0 },
+          { transform: 'translateY(-100px)', opacity: 0 },
           { transform: 'translateY(0)', opacity: 1 }
         ], {
           duration: 800 + index * 50,
@@ -15,16 +15,20 @@ const SimpleAnimation = () => {
         });
       }
     });
-  }, [keys]); // Dependency array to re-run the effect when keys change
+  }, [keys]);
 
   const keyPattern = ['white', 'black', 'white', 'black', 'white', 'white', 'black', 'white', 'black', 'white', 'black', 'white'];
-  const keyboardLength = 41; // Adjust for the desired length of the keyboard
+  const keyboardLength = 48;
   let keysElements = [];
+
+  // Calculate key dimensions based on paperSize
+  const keyWidth = paperSize.width / 13; // Example factor, adjust as needed
+  const keyHeight = paperSize.height / 1.3; // Example factor, adjust as needed
 
   for (let i = 0, w = 0; i < keyboardLength; i++) {
     const isWhite = keyPattern[i % keyPattern.length] === 'white';
-    const keyStyle = isWhite ? styles.whiteKey : styles.blackKey;
-    const leftPosition = isWhite ? `${2 + w * 3.5}vw` : `${2 + w * 3.5 - 1.25}vw`;
+    const keyStyle = isWhite ? { ...styles.whiteKey, width: keyWidth + 'px', height: keyHeight + 'px' } : { ...styles.blackKey, width: keyWidth * 0.7 + 'px', height: keyHeight * 0.6 + 'px' }; // Adjust black key dimensions accordingly
+    const leftPosition = isWhite ? `${w * keyWidth}px` : `${w * keyWidth - keyWidth * 0.35}px`;
 
     keysElements.push(
       <div
@@ -37,7 +41,7 @@ const SimpleAnimation = () => {
     if (isWhite) w++;
   }
 
-  useEffect(() => setKeys([...keysElements]), []); // Set keys on initial mount
+  useEffect(() => setKeys([...keysElements]), []);
 
   return (
     <div style={styles.animationContainer}>
@@ -47,42 +51,37 @@ const SimpleAnimation = () => {
   );
 };
 
-
 const styles = {
   animationContainer: {
     position: 'relative',
-    height: '20vw',
+    height: '200px', // Fixed height, adjust if needed
     width: '100%',
     zIndex: -1
   },
   whiteKey: {
-    width: '3.5vw',
-    height: '15vw',
     backgroundColor: 'white',
-    border: '0.1vw solid black',
+    border: '1px solid black',
     opacity: 0,
     position: 'absolute',
     top: '0',
-    zIndex: 0 // Z-index for white keys
+    zIndex: 0
   },
   blackKey: {
-    width: '2.5vw',
-    height: '8.5vw',
     backgroundColor: 'black',
     opacity: 0,
     position: 'absolute',
     top: '0',
-    zIndex: 1 // Higher Z-index for black keys
+    zIndex: 1
   },
   redBar: {
     position: 'absolute',
-    height: '2vw',
+    height: '20px',
     width: '100%',
     backgroundColor: 'darkred',
     top: 0,
     left: 0,
-    borderRadius: '0.5vw',
-    zIndex: 2 // Ensuring the red bar is on top of all keys
+    borderRadius: '5px',
+    zIndex: 2
   }
 };
 
--- piano_web/src/components/Home.js
+++ piano_web/src/components/Home.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { Container, Typography, Paper } from '@mui/material';
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
 import Animation from './Animation'; // Adjust the import path as needed
 import '../Style.css';
 
 const Home = () => {
+  const paperRef = useRef(null);
+  const [paperSize, setPaperSize] = useState({ width: 0, height: 0 });
+
+  useEffect(() => {
+    if (paperRef.current) {
+      const { offsetWidth, offsetHeight } = paperRef.current;
+      setPaperSize({ width: offsetWidth, height: offsetHeight });
+    }
+  }, [paperRef]);
+
   return (
     <Container maxWidth="md" style={{ marginTop: '20px' }}>
-      <Paper style={{ padding: '20px', textAlign: 'center', backgroundColor: '#212121', color: '#f5f5dc' }}>
+      <Paper ref={paperRef} style={{ padding: '20px', textAlign: 'center', backgroundColor: '#212121', color: '#f5f5dc', position: 'relative' }}>
+        <MusicNoteIcon style={styles.symbolStyle} />
+
         <Typography variant="h3" component="h1" style={{ color: '#f5f5dc' }}>
           Toronto Piano Tuning
         </Typography>
@@ -14,13 +27,25 @@ const Home = () => {
           Certified by <a href="https://music.uwo.ca/about/resources/pianos.html" style={{ color: '#f5f5dc' }} target="_blank" rel="noopener noreferrer">UWO</a>
         </Typography>
         <Typography variant="body1" style={{ color: '#f5f5dc', textAlign: 'center' }}>
-          Our mission is to bring quality and affordable tuning to pianists across the GTA.
+          Quality and affordable tuning for pianists across the GTA.
         </Typography>
       </Paper>
 
-      <Animation />
+      <Animation paperSize={paperSize} />
     </Container>
   );
+};
+
+const styles = {
+  symbolStyle: {
+    color: 'rgba(245, 245, 220, 0.2)',
+    fontSize: '10rem',
+    position: 'absolute',
+    top: '50%',
+    left: '10%',
+    transform: 'translateY(-50%)',
+    zIndex: 0
+  }
 };
 
 export default Home;
